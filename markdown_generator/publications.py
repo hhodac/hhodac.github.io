b8b1c3c6a5dff66acb2cdd240130aa32cf1dae7d
Hai Ho <haiho@Hais-MacBook-Pro.local>
2022-09-20 22:44:24 +1000

    correct typo

--- markdown_generator/publications.py
+++ markdown_generator/publications.py
@@ -83,7 +83,7 @@ for row, item in publications.iterrows():
     
     md += "\nvenue: '" + html_escape(item.venue) + "'"
 
-    md += "\journal: '" + html_escape(item.journal) + "'"
+    md += "\njournal: '" + html_escape(item.journal) + "'"
     
     if len(str(item.paper_url)) > 5:
         md += "\npaperurl: '" + item.paper_url + "'"
